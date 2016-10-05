aaee94a131c491935e04c208b11446aab8040f2a
eugkimStar <eugenekim@mail.weber.edu>
2016-10-05 14:53:17 -0600

    Fixed placeCard method and added a pile to SlapJackGame

--- app/src/main/java/Player.java
+++ app/src/main/java/Player.java
@@ -12,7 +12,9 @@ public class Player {
 
     public void placeCard(GroupOfCards pile)
     {
-        pile.getGroup().add((Hand.getGroup().get(0));
-        Hand.getGroup().remove(0);
+        if (Hand.getGroup().size() > 0) {
+            pile.getGroup().add(Hand.getGroup().get(0));
+            Hand.getGroup().remove(0);
+        }
     }
 }
