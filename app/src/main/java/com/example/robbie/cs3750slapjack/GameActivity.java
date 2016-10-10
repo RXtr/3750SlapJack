ed047c4039fdd22fc3973faa3444aa1664d0a62e
seth johns <seth johns>
2016-10-10 16:52:17 -0600

    more stuff and things

--- app/src/main/java/com/example/robbie/cs3750slapjack/GameActivity.java
+++ app/src/main/java/com/example/robbie/cs3750slapjack/GameActivity.java
@@ -9,26 +9,22 @@ import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
 
-    TextView text;
 
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        text = (TextView)findViewById(R.id.textView);
         Log.d("Did this work", "Maybe4");
         SlapJackGame objReceived = (SlapJackGame)((ObjectWrapperForBinder)getIntent()
                 .getExtras().getBinder("object_value")).getData();
         Log.d("Did this work", "received object=" + objReceived);
         if(objReceived.getPlayerCount() == 2){
-
+            getSupportFragmentManager().beginTransaction().add(R.id.);
         }
         else{
 
         }
-        text.setText("Player count" + objReceived.getPlayerCount()  + "\n" + objReceived.getPlayers().get(0).getName()
-            + " \n" + objReceived.getPlayers().get(1).getName());
 
     }
 }
