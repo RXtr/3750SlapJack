e4c2ef134537d75864d7117c96091b34990019dd
robbie <robbiextreme@gmail.com>
2016-10-11 11:37:59 -0600

    Added a setup method for getting players names and setting them to labels; refactored setup methods in GameActivity to onResume() override method.

--- app/src/main/java/com/example/robbie/cs3750slapjack/GameActivity.java
+++ app/src/main/java/com/example/robbie/cs3750slapjack/GameActivity.java
@@ -6,9 +6,12 @@ import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import java.util.ArrayList;
+
 public class GameActivity extends AppCompatActivity {
 
-    HandFragment[] players;
+    HandFragment[] playerBoards;
+    SlapJackGame game;
 
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
     @Override
@@ -21,54 +24,65 @@ public class GameActivity extends AppCompatActivity {
                 .getExtras().getBinder("object_value")).getData();
         Log.d("Did this work", "received object=" + objReceived);
 
-        if(objReceived.getPlayerCount() == 2) {
-            players = new HandFragment[2];
-            players[0] = new HandFragment();
-            players[1] = new HandFragment();
-            getSupportFragmentManager().beginTransaction().add(R.id.topRight, players[0], "playerOne").commit();
-            getSupportFragmentManager().beginTransaction().add(R.id.bottomRight, players[1], "playerTwo").commit();
+        if(game.getPlayerCount() == 2) {
+            playerBoards = new HandFragment[2];
+            playerBoards[0] = new HandFragment();
+            playerBoards[1] = new HandFragment();
+            getSupportFragmentManager().beginTransaction().add(R.id.topRight, playerBoards[0], "playerOne").commit();
+            getSupportFragmentManager().beginTransaction().add(R.id.bottomRight, playerBoards[1], "playerTwo").commit();
 
         }
         else {
-            players = new HandFragment[4];
-            players[0] = new HandFragment();
-            players[1] = new HandFragment();
-            players[2] = new HandFragment();
-            players[3] = new HandFragment();
-            getSupportFragmentManager().beginTransaction().add(R.id.topLeft, players[0], "playerOne").commit();
-            getSupportFragmentManager().beginTransaction().add(R.id.topRight, players[1], "playerTwo").commit();
-            getSupportFragmentManager().beginTransaction().add(R.id.bottomLeft, players[2], "playerThree").commit();
-            getSupportFragmentManager().beginTransaction().add(R.id.bottomRight, players[3], "playerFour").commit();
+            playerBoards = new HandFragment[4];
+            playerBoards[0] = new HandFragment();
+            playerBoards[1] = new HandFragment();
+            playerBoards[2] = new HandFragment();
+            playerBoards[3] = new HandFragment();
+            getSupportFragmentManager().beginTransaction().add(R.id.topLeft, playerBoards[0], "playerOne").commit();
+            getSupportFragmentManager().beginTransaction().add(R.id.topRight, playerBoards[1], "playerTwo").commit();
+            getSupportFragmentManager().beginTransaction().add(R.id.bottomLeft, playerBoards[2], "playerThree").commit();
+            getSupportFragmentManager().beginTransaction().add(R.id.bottomRight, playerBoards[3], "playerFour").commit();
 
         }
-
         getSupportFragmentManager().beginTransaction().add(R.id.centerLayout, new CenterFragment(), "pileFragment").commit();
+    }
 
-        highlightPlayer(objReceived.getSelectedPlayer());
-
-
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setPlayerBoards(game.getPlayers());
+        //highlightPlayer(game.getSelectedPlayer());
     }
 
     public void highlightPlayer(int player)
     {
         if(player == 0) {
-            players[0].highlight();
-            players[4].removeHighlight();
+            playerBoards[0].highlight();
+            playerBoards[4].removeHighlight();
         }
 
         else if(player == 1){
-            players[1].highlight();
-            players[0].removeHighlight();
+            playerBoards[1].highlight();
+            playerBoards[0].removeHighlight();
         }
 
         else if(player == 2){
-            players[2].highlight();
-            players[1].removeHighlight();
+            playerBoards[2].highlight();
+            playerBoards[1].removeHighlight();
         }
 
         else{
-            players[3].highlight();
-            players[2].removeHighlight();
+            playerBoards[3].highlight();
+            playerBoards[2].removeHighlight();
+        }
+    }
+
+    public void setPlayerBoards(ArrayList<Player> players)
+    {
+        for(int i = 0; i < players.size(); i++)
+        {
+            playerBoards[i].setPlayerLabel(players.get(i).getName().toString());
+            playerBoards[i].setCardCountLabel(players.get(i).getHand().CardCount());
         }
     }
 }
